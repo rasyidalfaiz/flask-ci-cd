77d97f6431e5467f6fe16ac81ca123896ee91b82
rasyidalfaiz <rasyidalfaiz73@gmail.com>
2025-07-29 05:01:52 +0700

    Deploy to Railway

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ def home():
     return render_template("index.html", db_status=db_status)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
