9a1fa0fd2084c0ec36ce917dfbc5335fc5d8fd30
rasyidalfaiz <rasyidalfaiz73@gmail.com>
2025-07-29 02:34:16 +0700

    Fix: update output to Hello from Flask with PostgreSQL

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def hello():
     return "Hello from Flask with PostgreSQL"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
