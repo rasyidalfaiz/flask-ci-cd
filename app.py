d40203a0b4f22b6aa240e72d40b5c2f322a3ab62
rasyidalfaiz <rasyidalfaiz73@gmail.com>
2025-07-29 02:30:57 +0700

    Fix: update output to Hello from Flask with PostgreSQL

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ app = Flask(__name__)
 def hello():
     conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
     cur = conn.cursor()
-    cur.execute("SELECT NOW()")
-    result = cur.fetchone()
+    cur.execute("SELECT 1")
+    cur.fetchone()
     cur.close()
     conn.close()
-    return f"Connected to DB! Current time: {result[0]}"
+    return "Hello from Flask with PostgreSQL"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True)
