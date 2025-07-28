6d62114594b96ddc5cf60552fcd0579d8b717589
rasyidalfaiz <rasyidalfaiz73@gmail.com>
2025-07-29 04:37:11 +0700

    Deploy: final layout + PostgreSQL env config

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 import psycopg2
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
-    cur = conn.cursor()
-    cur.execute("SELECT 1")
-    cur.fetchone()
-    cur.close()
-    conn.close()
-    return "Hello from Flask with PostgreSQL"
+def home():
+    try:
+        conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
+        cur = conn.cursor()
+        cur.execute("SELECT 1")
+        cur.fetchone()
+        cur.close()
+        conn.close()
+        db_status = "Connected to PostgreSQL!"
+    except Exception as e:
+        db_status = f"Connection failed: {e}"
+
+    return render_template("index.html", db_status=db_status)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-
+    app.run(debug=True)
