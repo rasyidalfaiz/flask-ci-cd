3b067dd10d654e104e1a6577a3718d2f35b6d020
rasyidalfaiz <rasyidalfaiz73@gmail.com>
2025-07-29 01:51:54 +0700

    Fix: create proper app.py file

---
+++ app.py/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
+    cur = conn.cursor()
+    cur.execute("SELECT NOW()")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f"Connected to DB! Current time: {result[0]}"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+    
